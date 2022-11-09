dd874c944009fe2aed25ab506782f6f2e285f71d
Kyle Fenlon <kylefenlon@Kyles-MacBook-Pro-2.local>
2022-11-09 16:13:05 +0000

    cube

--- Day_3/functions_lab_1/start_point/src/python_functions_practice.py
+++ Day_3/functions_lab_1/start_point/src/python_functions_practice.py
@@ -33,7 +33,33 @@ def add_string_as_number(num1, num2):
     return int(num1) + int(num2)
 
 def number_to_full_month_name(num):
-    return months[num]["fullname"]
+    if num == 1:
+        return "January"
+    if num == 2:
+        return "February"
+    if num == 3:
+        return "March"
+    if num == 4:
+        return "April"
+    if num == 5:
+        return "May"
+    if num == 6:
+        return "June"
+    if num == 7:
+        return "July"
+    if num == 8:
+        return "August"
+    if num == 9:
+        return "September"
+    if num == 10:
+        return "October"
+    if num == 11:
+        return "November"
+    if num == 12:
+        return "December"
+    
+    return None
+    
 
 def number_to_short_month_name(num):
     if num == 1:
@@ -62,5 +88,8 @@ def number_to_short_month_name(num):
         return "Dec"
     
     return None
+
+def cube(length):
+    return length * length * length
 
 
